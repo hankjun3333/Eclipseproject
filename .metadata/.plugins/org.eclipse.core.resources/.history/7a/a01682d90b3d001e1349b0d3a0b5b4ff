619846b485786a3c42b7abb8be844c393dd9b49e
hankjun3333 <hankjun3333@naver.com>
2023-08-18 00:52:15 +0900

    study

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/7a/a01682d90b3d001e1349b0d3a0b5b4ff
@@ -0,0 +1,9 @@
+package io;
+//flush close 필수!!
+public class file17 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- web/src/main/java/io/file18.java
+++ web/src/main/java/io/file18.java
@@ -1,5 +1,12 @@
 package io;
 
+import java.io.FileWriter;
+import java.io.*;
+import java.util.*;
+
+
+
+
 /*
   수욱제
   Scanner를 이용하여 총 10개의 숫자를 입력합니다!
@@ -13,7 +20,53 @@ public class file18 {
 
 	public static void main(String[] args) {
 		
+		try {
+			data15 d =new data15();
+			d.abc();
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		finally {
+			
+			try {
+				System.out.println("시스템 종료하였습니다.");
+				System.exit(0);
+			}
+			catch(Exception e) {
+				System.out.println(e);
+			}
+		}
 
 	}
 
 }
+class data15{
+	FileWriter fw =null;
+	public void abc() throws Exception{
+		this.fw = new FileWriter("D:\\project\\web\\src\\main\\webapp\\data_num.txt");
+		Scanner sc = new Scanner(System.in);
+		int w =0;
+		for(w=0; w<10; w++) {
+			System.out.println("1~100까지 숫자 중 한가지 입력하세요");
+			int data = sc.nextInt();
+			if(data<1 || data>100 ) {
+				System.out.println("범위에서 벗어났습니다!.");
+				w--;
+				continue;
+			}
+			//int를 문자로 바꿨는데 왜 InputMismatchException?
+			//아무글자나 쳐도 예외처리됨 이유가뭘까?
+			else if(String.valueOf(data) =="quit") { 
+				break;
+			}
+			else {
+				fw.write(data + "\n");
+			}
+		}
+		System.out.println("모든 데이터 저장완료");
+		sc.close();
+		fw.close();
+	}
+}
+
