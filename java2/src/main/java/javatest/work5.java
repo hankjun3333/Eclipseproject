d0d847b552baef62927898c956d89c89a22f662f
hankjun3333 <hankjun3333@naver.com>
2023-08-29 01:05:02 +0900

    study

--- java2/src/main/java/javatest/work5.java
+++ java2/src/main/java/javatest/work5.java
@@ -1,5 +1,7 @@
 package javatest;
 
+import java.util.*;
+
 /*
  숙제 상속형태의 클래스를 이용하여 다음 문제에 맞는 결과값을 돌출하는 코드를 작성하시오
  [DB] = {55,1,6,8,10,22,96,33,31,45,48,60}
@@ -15,8 +17,59 @@ package javatest;
 public class work5 {
 
 	public static void main(String[] args) {
-		
-
+		try {
+			new data5().db();
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 
+}
+class db{
+	String a[] = null;
+	public void db() {
+		this.a = new String[] {"55","1","6","8","10","22","96","33","31","45","48","60"};
+	}
+}
+class data5 extends db{
+	Scanner sc = null;
+	@Override
+	public void db() {
+		super.db();
+		
+		this.sc = new Scanner(System.in);
+		System.out.println("해당 데이터를 1.짝수, 2.홀수 를 선택하세요?: (기본값 - 짝수)");
+		String q1 = sc.nextLine().intern();
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		ArrayList<String> arr2 = new ArrayList<String>();
+		int ck=0;
+		if(q1 =="짝수") {
+			for(String num : this.a) {
+				if(Integer.parseInt(num)%2 ==0) {
+					//System.out.println(num);
+					arr.add(Integer.parseInt(num));
+				}
+			}
+			Collections.sort(arr); //정렬이 도데채 왜안될까
+			
+		}
+		else if(q1 =="홀수") {
+			for(String num : this.a) {
+				if(Integer.parseInt(num)%2 !=0) {
+					arr.add(Integer.parseInt(num));
+				}
+			}
+			Collections.sort(arr);
+		}
+		else {
+			while(true) {
+				System.out.println("값을 제대로 입력해주세요");
+				System.out.println("해당 데이터를 1.짝수, 2.홀수 를 선택하세요?: (기본값 - 짝수)");
+				q1 = sc.nextLine().intern();
+			}
+		}
+		System.out.println(arr);
+		
+	}
 }
